e15d5a76d13f545fb1160b84bbca2fed62a899fe
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-02-24 10:44:40 +0100

    seance5

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,13 +29,63 @@ class ArticleController extends AbstractController
         ]);
     }
     #[Route('/remove/{id}', name: 'remove3')]
-    public function remove(ManagerRegistry $mr,$id,ArticleRepository $repo): Response
+    public function remove(ManagerRegistry $mr, $id, ArticleRepository $repo): Response
     {
-        $a=$repo->find($id);
-$em=$mr->getManager();
-$em->remove($a);
-$em->flush();
-return new Response('removed');
+        $a = $repo->find($id);
+        $em = $mr->getManager();
+        $em->remove($a);
+        $em->flush();
+        //return new Response('removed');
+        return $this->redirectToRoute('getarticle');
+    }
+    #[Route('/addarticle', name: 'addarticle')]
+    public function addArticle(ManagerRegistry $mr, Request $req): Response
+    {
+        $a = new Article();
+        //$a->setref(123456789);
+        //$a->setTitre('fvfdgg');
+        $f = $this->createForm(ArticleType::class, $a);
+        $f->handleRequest($req);
+       // dd($req);
         
+        if ($f->isSubmitted()) {
+            $a->setCreatedAt(new DateTimeImmutable('now'));
+           // dd($a);
+          $ref=$mr->getRepository(Article::class)->find($a->getref());
+          //dd($ref);
+          $titre=$a->getref().'@esprit.tn';
+          $a->setTitre($titre);
+          if($ref==null){
+            $em = $mr->getManager();
+            $em->persist($a);
+            $em->flush();
+            return $this->redirectToRoute('getarticle');
+        } else{
+return new Response('ref deja existe');
+        }
+            
+        }
+        return $this->renderForm('article/addarticle.html.twig', [
+            'formarticle' => $f
+        ]);
+    }
+    #[Route('/update/{id}', name: 'update')]
+    public function updateArticle(ManagerRegistry $mr, Request $req,$id): Response
+    {
+      //  $a = new Article();
+   $a=$mr->getRepository(Article::class)->find($id);
+        $f = $this->createForm(ArticleType::class, $a);
+        $f->handleRequest($req);
+          
+        if ($f->isSubmitted()) {
+            //$a->setCreatedAt(new DateTimeImmutable('now'));
+            $em = $mr->getManager();
+            //$em->persist($a);
+            $em->flush();
+            return $this->redirectToRoute('getarticle');
+        }
+        return $this->renderForm('article/addarticle.html.twig', [
+            'formarticle' => $f
+        ]);
     }
 }
---
+++ src/Form/UpdatearticleType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdatearticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            //->add('ref')
+            ->add('titre')
+            ->add('createdAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
